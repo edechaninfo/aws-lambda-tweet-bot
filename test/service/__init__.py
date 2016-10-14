8b397b8870aafd539b2df33405fddbd06fa2d844
edechaninfo <edechaninfo@gmail.com>
2016-10-15 08:14:53 +0900

    Make unit test framework
    Do just 'python setup.py test' for unit tests

--- test/service/__init__.py
+++ test/service/__init__.py
@@ -11,14 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-import setuptools
-
-
-setuptools.setup(name='aws_lambda_tweet_bot',
-                 version='0.1',
-                 author='edechaninfo',
-                 author_email='edechaninfo@gmail.com',
-                 packages=setuptools.find_packages(),
-                 test_suite='test')
