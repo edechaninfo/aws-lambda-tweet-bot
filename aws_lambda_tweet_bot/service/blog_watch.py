46a8532f6de1a5add2977f330d7e709a85c6928e
edechaninfo <edechaninfo@gmail.com>
2017-02-05 23:00:24 -0800

    Ameblo Blog Watch is Implemented
    Resolves #12

--- aws_lambda_tweet_bot/service/blog_watch.py
+++ aws_lambda_tweet_bot/service/blog_watch.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from HTMLParser import HTMLParser
 import logging
 
 import feedparser
+import requests
 
 from tweepy import TweepError
 from aws_lambda_tweet_bot.utils import get_dynamodb_table, get_tweepy_api
@@ -25,6 +27,76 @@ logger = logging.getLogger('aws_lambda_tweet_bot.service.blog_watch')
 logger.setLevel(logging.INFO)
 
 
+class AmebloBodyParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.in_main_body = False
+        self.count_div_in = 0
+        self.articleText = ""
+        self.article_classes = ['articleText', 'skin-entryBody']
+
+    # handle unicode error in ameblo
+    def parse_starttag(self, i):
+        try:
+            return HTMLParser.parse_starttag(self, i)
+        except UnicodeDecodeError:
+            pass
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            if self.in_main_body:
+                self.count_div_in += 1
+            else:
+                for attr in attrs:
+                    key, val = attr
+                    if key == 'class' and val in self.article_classes:
+                        self.in_main_body = True
+
+    def handle_data(self, data):
+        if self.in_main_body:
+            self.articleText += data
+
+    def handle_endtag(self, tag):
+        if tag == 'div' and self.in_main_body:
+            if self.count_div_in > 0:
+                self.count_div_in -= 1
+            else:
+                self.in_main_body = False
+
+
+def _blog_body(link):
+    # stab
+    try:
+        r = requests.get(link)
+        parser = AmebloBodyParser()
+        parser.feed(r.text)
+        parser.close()
+        return parser.articleText
+    except Exception as e:
+        # catch all unexpected exception to avoid halting all search
+        errmsg = "Unexpected error on parsing blog {}: Msg -> {}"
+        logger.error(errmsg.format(link, str(e)))
+        return ""
+
+
+def _match_search_condition(db_item, feed_entry, latest_id):
+    match = False
+    # Check if this entry should be searched
+    if latest_id is None or latest_id < feed_entry.id:
+        title_search_condition = db_item.get('search_condition', None)
+        if title_search_condition is not None:
+            # title match
+            match = title_search_condition in feed_entry.title
+        if not match and db_item.get('body_search', False):
+            # blog body match
+            body = _blog_body(feed_entry.link)
+            for cond in db_item.get('body_search_conditions', []):
+                if cond in body:
+                    match = True
+                    break
+    return match
+
+
 def bot_handler(env, conf):
     logger.info('service "{}" started!"'.format(SERVICE_ID))
     blog_table = get_dynamodb_table('blog_watch', conf)
@@ -33,13 +105,12 @@ def bot_handler(env, conf):
         try:
             feed_url = blog_item.get('feed', "")
             latest_id = blog_item.get('latest_id', None)
-            updated = False
+            tw_fail = False
             news_dic = feedparser.parse(feed_url)
             if len(news_dic['entries']) <= 0:
                 raise Exception("No entries. Please check feed url setting.")
             for entry in news_dic['entries']:
-                if ((latest_id is None or latest_id < entry.id) and
-                        blog_item.get('search_condition', '') in entry.title):
+                if _match_search_condition(blog_item, entry, latest_id):
                     if not blog_item.get('body_format'):
                         raise KeyError("body_format must be defined")
 
@@ -52,23 +123,23 @@ def bot_handler(env, conf):
                                         errKey)
                     api = get_tweepy_api(env['twitter_env'], conf)
 
-                    tw_success = True
+                    # tw_success = True
                     try:
                         api.update_status(twbody)
-                        updated = True
+                        # updated = True
                         logger.info('Tweet Success!\n' +
                                     twbody.encode('utf_8'))
                     except TweepError as e:
                         if 'Status is a duplicate.' not in e.reason:
-                            tw_success = False
+                            tw_fail = True
                             logger.error(str(e))
-                    if tw_success and (blog_item.get('latest_id') is None or
-                                       blog_item['latest_id'] < entry.id):
-                        blog_item['latest_id'] = entry.id
-            if updated:
+                if (blog_item.get('latest_id') is None or
+                        blog_item['latest_id'] < entry.id):
+                    blog_item['latest_id'] = entry.id
+            if not tw_fail:
                 blog_table.put_item(Item=blog_item)
             else:
-                logger.info("No blog update")
+                logger.info("Tweet is failed. Retry next attempt.")
         except Exception as e:
             errmsg = "Unexpected error on service {}: " + \
                      "Please check DB record (id: {}): Msg -> {}"
--- test/service/test_blog_watch.py
+++ test/service/test_blog_watch.py
@@ -15,14 +15,19 @@
 from mock import patch
 import unittest
 
+from requests.exceptions import ConnectionError
+
 from config import Config
 from aws_lambda_tweet_bot.service import blog_watch
 from test import FakeDynamodbTable, FakeTweepyApi, FakeLogger
-from test.service.sample_data import sample_blog_data, sample_blog_data2
+from test.service.sample_data import sample_blog_data, sample_blog_data2, \
+    sample_blog_data3, sample_ameblo_blog_body
+from test.utils import obj
 
 
-D_TARGET = "aws_lambda_tweet_bot.service.blog_watch.get_dynamodb_table"
-T_TARGET = "aws_lambda_tweet_bot.service.blog_watch.get_tweepy_api"
+PATH_BLOG_WATCH = "aws_lambda_tweet_bot.service.blog_watch"
+D_TARGET = PATH_BLOG_WATCH + ".get_dynamodb_table"
+T_TARGET = PATH_BLOG_WATCH + ".get_tweepy_api"
 
 
 class FakeFeedparser(object):
@@ -36,6 +41,25 @@ class FakeFeedparser(object):
 
     def parse(self, url):
         return self.results.get(url, self.DEFAULT_RETURN)
+
+
+class FakeRequests(object):
+    def __init__(self, url_body_pairs={}):
+        self.url_body_pairs = url_body_pairs
+
+    def get(self, url):
+        class FakeResponseClass(object):
+            def __init__(self, body):
+                self._text = body
+
+            @property
+            def text(self):
+                return self._text
+        body = self.url_body_pairs.get(url)
+        if body is not None:
+            return FakeResponseClass(body)
+        else:
+            raise ConnectionError()
 
 
 class TestBlogWatch(unittest.TestCase):
@@ -72,7 +96,7 @@ class TestBlogWatch(unittest.TestCase):
         self.assertEqual(2, len(tw._update_statuses))
         item = dynamo.get_item(feed_item["id"])
         self.assertEqual(
-            "http://ameblo.jp/fruits-box-blog/entry-12210698546.html",
+            "http://ameblo.jp/fruits-box-blog/entry-12211554968.html",
             item['latest_id'])
 
         result_status_1 = "[New Update] Ede: Yoru-night! -> " + \
@@ -112,7 +136,7 @@ class TestBlogWatch(unittest.TestCase):
         # For blog 1
         item = dynamo.get_item(feed_item_1["id"])
         self.assertEqual(
-            "http://ameblo.jp/fruits-box-blog/entry-12210698546.html",
+            "http://ameblo.jp/fruits-box-blog/entry-12211554968.html",
             item['latest_id'])
         # For blog 2
         item = dynamo.get_item(feed_item_2["id"])
@@ -162,7 +186,7 @@ class TestBlogWatch(unittest.TestCase):
                       self.logger.lines_dict['error'][0])
 
     def test_empty_search_condition(self):
-        # Empty search_condition is regarded as all conditions are passed
+        # Empty search_condition is regarded as all conditions are rejected
         feed_item = dict(
             id="feed",
             feed="http://existsite.io/feed",
@@ -176,7 +200,7 @@ class TestBlogWatch(unittest.TestCase):
         env = {'twitter_env': 'test'}
 
         self._bot_handler(env, self.config, tw, dynamo, feed)
-        self.assertEqual(3, len(tw._update_statuses))
+        self.assertEqual(0, len(tw._update_statuses))
         item = dynamo.get_item(feed_item["id"])
         self.assertEqual(
             "http://ameblo.jp/fruits-box-blog/entry-12211554968.html",
@@ -237,7 +261,7 @@ class TestBlogWatch(unittest.TestCase):
         self.assertEqual(2, len(tw._update_statuses))
         item = dynamo.get_item(feed_item["id"])
         self.assertEqual(
-            "http://ameblo.jp/fruits-box-blog/entry-12210698546.html",
+            "http://ameblo.jp/fruits-box-blog/entry-12211554968.html",
             item['latest_id'])
 
     def test_no_new_blog_update(self):
@@ -245,7 +269,7 @@ class TestBlogWatch(unittest.TestCase):
             id="feed",
             feed="http://existsite.io/feed",
             latest_id="http://ameblo.jp/fruits-box-blog/"
-                      "entry-12211554968.html",
+                      "entry-12211554966.html",
             body_format="[New Update] {title} -> {link}"
         )
         tw = FakeTweepyApi()
@@ -255,7 +279,33 @@ class TestBlogWatch(unittest.TestCase):
 
         self._bot_handler(env, self.config, tw, dynamo, feed)
         self.assertEqual(0, len(tw._update_statuses))
-        self.assertEqual("No blog update", self.logger.lines_dict['info'][-1])
+        # No update, but id is moved to latest id
+        item = dynamo.get_item(feed_item["id"])
+        self.assertEqual(
+            "http://ameblo.jp/fruits-box-blog/entry-12211554968.html",
+            item['latest_id'])
+
+    def test_update_fail_no_index_change(self):
+        feed_item = dict(
+            id="feed",
+            feed="http://existsite.io/feed",
+            latest_id="http://ameblo.jp/fruits-box-blog/"
+                      "entry-12208663602.html",
+            search_condition="Ede:",
+            body_format="[New Update] {title} -> {link}"
+        )
+        tw = FakeTweepyApi(update_error=True)
+        dynamo = FakeDynamodbTable([feed_item])
+        feed = FakeFeedparser({"http://existsite.io/feed": sample_blog_data})
+        env = {'twitter_env': 'test'}
+
+        self._bot_handler(env, self.config, tw, dynamo, feed)
+        self.assertEqual(0, len(tw._update_statuses))
+        # Tweet error, so latest id is not moved
+        item = dynamo.get_item(feed_item["id"])
+        self.assertEqual(
+            "http://ameblo.jp/fruits-box-blog/entry-12208663602.html",
+            item['latest_id'])
 
     def test_empty_blogs(self):
         tw = FakeTweepyApi()
@@ -281,6 +331,218 @@ class TestBlogWatch(unittest.TestCase):
         self.assertEqual(0, len(tw._update_statuses))
         self.assertIn('No entries. Please check feed url setting.',
                       self.logger.lines_dict['error'][0])
+
+    def test_second_search_do_nothing(self):
+        feed_item = dict(
+            id="feed",
+            feed="http://existsite.io/feed",
+            latest_id="http://ameblo.jp/fruits-box-blog/"
+                      "entry-12208663600.html",
+            search_condition="Ede:",
+            body_format="[New Update] {title} -> {link} #Ede-chan"
+        )
+        tw = FakeTweepyApi()
+        dynamo = FakeDynamodbTable([feed_item])
+        feed = FakeFeedparser({"http://existsite.io/feed": sample_blog_data})
+        env = {'twitter_env': 'test'}
+
+        self._bot_handler(env, self.config, tw, dynamo, feed)
+        item = dynamo.get_item(feed_item["id"])
+        self.assertEqual(
+            "http://ameblo.jp/fruits-box-blog/entry-12211554968.html",
+            item['latest_id'])
+
+        # def fake function for _match_search_condition
+        def _fake_msc(db_item, feed_entry, latest_id):
+            match = False
+            # Check if this entry should be searched
+            if latest_id is None or latest_id < feed_entry.id:
+                raise Exception("2nd try has come")
+            return match
+        # blog_watch._match_search_condition = _fake_match_search_condition
+        with patch(PATH_BLOG_WATCH + '._match_search_condition', _fake_msc):
+            self._bot_handler(env, self.config, tw, dynamo, feed)
+        self.assertEqual(0, len(self.logger.lines_dict['error']))
+
+    def test_blog_body_search_mock(self):
+        feed_item = dict(
+            id="feed",
+            feed="http://existsite.io/feed",
+            latest_id="http://ameblo.jp/ari-step/entry-12224195010.html",
+            body_search=True,
+            body_search_conditions=["Hondo-chan"],
+            body_format="[New Update] {title} -> {link} #Ari-chan #Ede-chan"
+        )
+        tw = FakeTweepyApi()
+        dynamo = FakeDynamodbTable([feed_item])
+        feed = FakeFeedparser({"http://existsite.io/feed": sample_blog_data3})
+        req = FakeRequests(
+            {'http://ameblo.jp/ari-step/entry-12226218315.html':
+             sample_ameblo_blog_body})
+        env = {'twitter_env': 'test'}
+
+        with patch(PATH_BLOG_WATCH + '.requests', req):
+            self._bot_handler(env, self.config, tw, dynamo, feed)
+        result_status = "[New Update] Look this -> " + \
+            "http://ameblo.jp/ari-step/entry-12226218315.html " + \
+            "#Ari-chan #Ede-chan"
+        self.assertEqual(1, len(tw._update_statuses))
+        self.assertIn(result_status, tw._update_statuses)
+
+    def test_blog_body_search_mock_no_match(self):
+        # '4DX' word is found in link list but this should not be tweeted
+        feed_item = dict(
+            id="feed",
+            feed="http://existsite.io/feed",
+            latest_id="http://ameblo.jp/ari-step/entry-12224195010.html",
+            body_search=True,
+            body_search_conditions=["4DX"],
+            body_format="[New Update] {title} -> {link} #Ari-chan #Ede-chan"
+        )
+        tw = FakeTweepyApi()
+        dynamo = FakeDynamodbTable([feed_item])
+        feed = FakeFeedparser({"http://existsite.io/feed": sample_blog_data3})
+        req = FakeRequests(
+            {'http://ameblo.jp/ari-step/entry-12226218315.html':
+             sample_ameblo_blog_body})
+        env = {'twitter_env': 'test'}
+
+        with patch(PATH_BLOG_WATCH + '.requests', req):
+            self._bot_handler(env, self.config, tw, dynamo, feed)
+        self.assertEqual(0, len(tw._update_statuses))
+
+    def test_blog_body_search_online(self):
+        # Check if Ameblo spec is not changed
+        # Yui Ogura Blog Data
+        ogusan_blog_data = {
+            'bozo_exception': {
+                'message': 'document declared as us-ascii, but parsed as utf-8'
+            },
+            'encoding': 'utf-8',
+            'entries': [
+                obj(id='http://ameblo.jp/ogurayui-0815/entry-12187666106.html',
+                    title='Supply Water',
+                    link='http://ameblo.jp/ogurayui-0815/'
+                         'entry-12187666106.html'),
+                obj(id='http://ameblo.jp/ogurayui-0815/entry-12186656735.html',
+                    title='Hot and Spicy',
+                    link='http://ameblo.jp/ogurayui-0815/'
+                         'entry-12186656735.html'),
+                obj(id='http://ameblo.jp/ogurayui-0815/entry-12186044375.html',
+                    title='Summer Vacation',
+                    link='http://ameblo.jp/ogurayui-0815/'
+                         'entry-12186044375.html')
+            ]
+        }
+        kyarisan_blog_data = {
+            'bozo_exception': {
+                'message': 'document declared as us-ascii, but parsed as utf-8'
+            },
+            'encoding': 'utf-8',
+            'entries': [
+                obj(id='http://ameblo.jp/ishiharakaori-0806/'
+                        'entry-12225358373.html',
+                    title='Pop in Q',
+                    link='http://ameblo.jp/ishiharakaori-0806/'
+                         'entry-12225358373.html'),
+                obj(id='http://ameblo.jp/ishiharakaori-0806/'
+                    'entry-12224315509.html',
+                    title='Cocon Poi Poi Cocotama',
+                    link='http://ameblo.jp/ishiharakaori-0806/'
+                         'entry-12224315509.html'),
+                obj(id='http://ameblo.jp/ishiharakaori-0806/'
+                    'entry-12223186447.html',
+                    title='Event',
+                    link='http://ameblo.jp/ishiharakaori-0806/'
+                         'entry-12223186447.html')
+            ]
+        }
+        tanoway_blog_data = {
+            'bozo_exception': {
+                'message': 'document declared as us-ascii, but parsed as utf-8'
+            },
+            'encoding': 'utf-8',
+            'entries': [
+                obj(id='http://ameblo.jp/tanoue-marina/entry-12232581672.html',
+                    title='Birthday',
+                    link='http://ameblo.jp/tanoue-marina/'
+                         'entry-12232581672.html'),
+                obj(id='http://ameblo.jp/tanoue-marina/entry-12232298510.html',
+                    title='VR',
+                    link='http://ameblo.jp/tanoue-marina/'
+                         'entry-12232298510.html'),
+                obj(id='http://ameblo.jp/tanoue-marina/entry-12231980149.html',
+                    title='Greeting 2',
+                    link='http://ameblo.jp/tanoue-marina/'
+                         'entry-12231980149.html')
+            ]
+        }
+        arichan_blog_data = {
+            'bozo_exception': {
+                'message': 'document declared as us-ascii, but parsed as utf-8'
+            },
+            'encoding': 'utf-8',
+            'entries': [
+                obj(id='http://ameblo.jp/ari-step/entry-12233144905.html',
+                    title='In these days...',
+                    link='http://ameblo.jp/ari-step/entry-12233144905.html'),
+                obj(id='http://ameblo.jp/ari-step/entry-12226218315.html',
+                    title='Look this',
+                    link='http://ameblo.jp/ari-step/entry-12226218315.html'),
+                obj(id='http://ameblo.jp/ari-step/entry-12224195011.html',
+                    title='4DX!!',
+                    link='http://ameblo.jp/ari-step/entry-12224195011.html')
+            ]
+        }
+        ogusan_feed_item = dict(
+            id="ogusan",
+            feed="http://existsite.io/ogusan",
+            latest_id="http://ameblo.jp/ogurayui-0815/entry-12184451116.html",
+            body_format="[New Update] {title} -> {link} #Yui-chan #Ede-chan",
+            body_search=True,
+            body_search_conditions=[u"\u3048\u30fc\u3067\u3061\u3083\u3093"]
+            # edechan in Hiragana
+        )
+        kyarisan_feed_item = dict(
+            id="kyarisan",
+            feed="http://existsite.io/kyarisan",
+            latest_id="http://ameblo.jp/ishiharakaori-0806/"
+                      "entry-12223056549.html",
+            body_format="[New Update] {title} -> {link} #Kyari-san #Ede-chan",
+            body_search=True,
+            body_search_conditions=[u"\u672c\u6e21"]
+            # hondo in Kanji
+        )
+        tanoway_feed_item = dict(
+            id="tanoway",
+            feed="http://existsite.io/tanoway",
+            latest_id="http://ameblo.jp/tanoue-marina/entry-12231636584.html",
+            body_format="[New Update] {title} -> {link} #Tano-way #Ede-chan",
+            body_search=True,
+            body_search_conditions=[u"\u672c\u6e21"]
+            # hondo in Kanji
+        )
+        arichan_feed_item = dict(
+            id="tanoway",
+            feed="http://existsite.io/arichan",
+            latest_id="http://ameblo.jp/ari-step/entry-12223871525.html",
+            body_format="[New Update] {title} -> {link} #Ari-chan #Ede-chan",
+            body_search=True,
+            body_search_conditions=[u"\u672c\u6e21"]
+            # hondo in Kanji
+        )
+        tw = FakeTweepyApi()
+        dynamo = FakeDynamodbTable([ogusan_feed_item, kyarisan_feed_item,
+                                    tanoway_feed_item, arichan_feed_item])
+        feed = FakeFeedparser(
+            {"http://existsite.io/ogusan": ogusan_blog_data,
+             "http://existsite.io/kyarisan": kyarisan_blog_data,
+             "http://existsite.io/tanoway": tanoway_blog_data,
+             "http://existsite.io/arichan": arichan_blog_data})
+        env = {'twitter_env': 'test'}
+
+        self._bot_handler(env, self.config, tw, dynamo, feed)
+        self.assertEqual(5, len(tw._update_statuses))
 
 
 if __name__ == '__main__':
